84cd5f7d0b49f6e1b57b9f4f91a93d5833bf433f
Eric Anderson <eanders@microsoft.com>
2019-02-07 10:32:28 -0800

    wip

--- samples/solar-system/src/server.ts
+++ samples/solar-system/src/server.ts
@@ -4,15 +4,20 @@
  */
 
 import { WebHost } from '@microsoft/mixed-reality-extension-sdk';
+import { log } from '@microsoft/mixed-reality-extension-sdk/built/log';
 import { resolve as resolvePath } from 'path';
 import SolarSystem from './app';
+
+log.enable('app');
+// log.enable('network', 'debug');
 
 process.on('uncaughtException', err => console.log('uncaughtException', err));
 process.on('unhandledRejection', reason => console.log('unhandledRejection', reason));
 
 // Start listening for connections, and serve static files
 const server = new WebHost({
-    baseDir: resolvePath(__dirname, '../public')
+    baseDir: resolvePath(__dirname, '../public'),
+//    baseUrl: 'http://915514d2.ngrok.io'
 });
 
 // Handle new application sessions
